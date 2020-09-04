fcb3309de0f7ab4812bb8b9247ab71fe13533226
davidbrucetn <davidbrucetn@gmail.com>
2020-09-04 10:55:49 -0500

    In class example of protected class access modifier

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MoreClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog roy = new Dog();
+            roy.Name = "Roy";
+
+            Console.WriteLine($"Name: {roy.Name}");
+            string royString = roy.ToString();
+            Console.WriteLine($"ToString of object: {royString}");
+            Console.WriteLine($"Automatically calls ToString Method: {roy}");
+
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            // Can't access Species if private in child class
+            // If Protected you can
+            Species = "Dog";
+            return $"My Name is {Name}. I'm a {Species}.";
+        }
+    }
+
+    public class Animal
+    {
+        public string Species { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# nss-accessmodifiers-MoreClasses
+### Book 1 Ch 25 More Classes -> Access Modifiers PROTECTED
